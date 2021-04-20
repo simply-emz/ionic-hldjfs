429c294af95d2fbfbd2fb2b04aeb7d8feb9d802b
simply-emz <emie.zipagan@lsgh.edu.ph>
2021-04-20 19:52:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-hldjfs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-hldjfs)
---
+++ pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
+})
+export class HomePage {
+  feet: number;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
